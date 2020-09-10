48a2af56b564afcf540ca4ce1b40632c40eedbbd
danielkreiter <daniel.kreiter@gmx.de>
2020-09-10 17:35:25 +0200

    Add quiz repository

---
+++ lib/persistence/repositorys/datasource/quiz_dto/quiz_dto_repository.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:basetalk/domain/entities/media.dart';
+import 'package:basetalk/persistence/dto/quiz_data_dto.dart';
+import 'package:basetalk/persistence/repositorys/datasource/interfaces/i_quiz_dto_repository.dart';
+
+class QuizDTORepository implements IQuizDTORepository {
+  Future<QuizDataDTO> getQuizDataDTO(Media quizFileMedia) async {
+    String jsonString = await quizFileMedia.file.readAsString();
+    QuizDataDTO topicDtos = jsonDecode(jsonString) as QuizDataDTO;
+    return topicDtos;
+  }
+}
