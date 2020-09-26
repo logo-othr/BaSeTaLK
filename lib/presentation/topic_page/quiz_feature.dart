d2f523ce819c707172360ba329b6253bad064508
kreiterd <daniel.kreiter@gmx.de>
2020-09-26 19:32:45 +0200

    QuizFeature dynamic size

--- lib/presentation/topic_page/quiz_feature.dart
+++ lib/presentation/topic_page/quiz_feature.dart
@@ -3,9 +3,9 @@ import 'package:basetalk/domain/entities/quiz_data.dart';
 import 'package:basetalk/domain/entities/quiz_question.dart';
 import 'package:basetalk/presentation/colors.dart';
 import 'package:basetalk/presentation/topic_page/viewmodel/quiz_view_model.dart';
+import 'package:basetalk/presentation/topic_page/viewmodel/topic_page_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 
 class QuizFeature extends StatefulWidget {
   QuizFeature();
@@ -41,6 +41,42 @@ class _QuizFeatureState extends State<QuizFeature> {
   Widget build(BuildContext context) {
     List<Widget> questionWidgets = List();
     Widget firstQuizPage = Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 30),
+          child: Text(
+            "Zeit für ein Quiz",
+            style: TextStyle(
+              fontSize: 36,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+        Text(
+          "Wie lauten die folgenden Redewendungen?",
+          style: TextStyle(fontSize: 36),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+
+    Widget closeButton = FlatButton(
+      height: 70,
+      minWidth: 300,
+      child: Text(
+        "Quiz beenden",
+        style: TextStyle(fontSize: 28, color: Colors.white),
+      ),
+      color: Colors.grey[600],
+      onPressed: () {
+        Provider.of<TopicPageViewModel>(context, listen: false)
+            .toggleFeatureVisible();
+      },
+    );
+
+    Widget lastQuizPage = Column(
       children: [
         Text(
           "Zeit für ein Quiz",
@@ -50,16 +86,27 @@ class _QuizFeatureState extends State<QuizFeature> {
           ),
         ),
         Spacer(),
-        Text(
-          "Wie lauten die folgenden Redewendungen?",
-          style: TextStyle(fontSize: 36),
-          textAlign: TextAlign.center,
-        ),
+        closeButton,
         Spacer(),
       ],
     );
     questionWidgets.add(firstQuizPage);
 
+    Widget nextButton = FlatButton(
+      height: 70,
+      minWidth: 300,
+      child: Text(
+        "Weiter",
+        style: TextStyle(fontSize: 28, color: Colors.white),
+      ),
+      color: Colors.grey[600],
+      onPressed: () {
+        _controller.nextPage(
+            duration: Duration(milliseconds: 300), curve: Curves.linear);
+        Provider.of<QuizViewModel>(context, listen: false)
+            .setNextButtonVisibility(false);
+      },
+    );
 
     return AspectRatio(
       aspectRatio: 4 / 3,
@@ -73,37 +120,30 @@ class _QuizFeatureState extends State<QuizFeature> {
               for (QuizQuestion quizQuestion in snapshot.data.questions) {
                 questionWidgets.add(quizContainer(quizQuestion));
               }
+              questionWidgets.add(lastQuizPage);
               return Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
                   Expanded(
-                      child: PageView(
-                          physics: new NeverScrollableScrollPhysics(),
-                          controller: _controller,
-                          children: questionWidgets)),
+                    child: PageView.builder(
+                      itemCount: questionWidgets.length,
+                      physics: new NeverScrollableScrollPhysics(),
+                      controller: _controller,
+                      itemBuilder: (context, index) {
+                        return questionWidgets[index];
+                      },
+                    ),
+                  ),
                   Provider
                       .of<QuizViewModel>(context)
                       .isNextButtonVisible
-                      ? FlatButton(
-                    padding: EdgeInsets.all(20),
-                    child: Text(
-                      "Weiter",
-                      style: TextStyle(fontSize: 35, color: Colors.white),
-                    ),
-                    color: Colors.grey[600],
-                    onPressed: () {
-                      _controller.nextPage(
-                          duration: Duration(milliseconds: 300),
-                          curve: Curves.linear);
-                      Provider.of<QuizViewModel>(context, listen: false)
-                          .setNextButtonVisibility(false);
-                    },
-                  )
-                      : Container(),
+                      ? nextButton
+                      : Container(
+                    height: 70,
+                  ),
                 ],
               );
-            }
-            else {
+            } else {
               return Container();
             }
           },
@@ -130,23 +170,32 @@ class _QuizFeatureState extends State<QuizFeature> {
       buttons.add(QuizButton(activatedColor, quizAnswer.answer, onPressed));
     }
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          quizQuestion.question,
-          style: TextStyle(fontSize: 36),
+    return Center(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxHeight: 400),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              quizQuestion.question,
+              style: TextStyle(fontSize: 28),
+            ),
+            SizedBox(height: 18),
+            Expanded(
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(14),
+                  child: Column(
+                    children: buttons
+                        .map((w) => Expanded(child: w))
+                        .toList(),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
-        SizedBox(height: 40),
-        Container(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: buttons
-                .map((w) => Container(child: w, padding: EdgeInsets.all(6)))
-                .toList(),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
@@ -167,23 +216,26 @@ class _QuizButtonState extends State<QuizButton> {
 
   @override
   Widget build(BuildContext context) {
-    return ButtonTheme(
-      minWidth: 48,
-      child: Container(
-        width: double.infinity,
-        height: 60,
-        child: FlatButton(
-          child: Text(
-            widget.buttonText,
-            style: TextStyle(fontSize: 30),
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: ButtonTheme(
+        minWidth: 48,
+        child: Container(
+          width: double.infinity,
+          height: 55,
+          child: FlatButton(
+            child: Text(
+              widget.buttonText,
+              style: TextStyle(fontSize: 28),
+            ),
+            color: pressed ? widget.activatedColor : primary_green,
+            onPressed: () {
+              setState(() {
+                pressed = true;
+              });
+              widget.onPressed();
+            },
           ),
-          color: pressed ? widget.activatedColor : primary_green,
-          onPressed: () {
-            setState(() {
-              pressed = true;
-            });
-            widget.onPressed();
-          },
         ),
       ),
     );
